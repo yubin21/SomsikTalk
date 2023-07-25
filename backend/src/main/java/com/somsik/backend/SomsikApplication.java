d0f8452b8d69aca15cd15db188c19fa1311a932f
yubin <garmalhy1@gmail.com>
2023-07-25 13:23:23 +0900

    [FEAT] spring init

---
+++ backend/src/main/java/com/somsik/backend/SomsikApplication.java
@@ -0,0 +1,13 @@
+package com.somsik.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomsikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomsikApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/somsik/backend/SomsikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somsik.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomsikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
