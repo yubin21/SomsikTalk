eadaab04d7b8611e643cb5cc6d728c70da204c4e
devyu <80163835+yubin21@users.noreply.github.com>
2023-10-21 13:14:22 +0900

    [FEAT] add html and css

---
+++ src/main/java/com/somsik/backend/somsiktalk/board/BoardController.java
@@ -0,0 +1,59 @@
+package com.somsik.backend.somsiktalk.board;
+
+import com.somsik.backend.somsiktalk.domain.item.Item;
+import com.somsik.backend.somsiktalk.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board/items")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "board/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "board/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "board/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/board/items/{itemId}";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "board/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+}
